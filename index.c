a98c02b5faa276a63b5dbbc48a9fa5eecc3e67ae
Hanapina <jewli@ucdavis.edu>
2020-11-12 13:56:52 -0800

    trying to see if string plugins for functions work.

--- index.c
+++ index.c
@@ -19,12 +19,12 @@ void intro(){
 	 PROB THE ONE THAT WILL CREATE THE MOST PROBLEMS ATM.
 	 Using vectors to organize > write back to the file.
 	 Current write_back uses sep files to check for issues. */
-void organize_list() {
+void organize_list(string fileName) {
 	string name;
 	vector<string> name_vector;
 
 	// Reading first and then making vector.
-	ifstream myfile("anime_index.txt");
+	ifstream myfile(fileName);
 	while (getline(myfile, name)) {
 		name_vector.push_back(name);
 	}
@@ -38,7 +38,7 @@ void organize_list() {
 	*/
 
 	// Writing into textfile via from vector and then closing file
-	ofstream writefile("anime_index.txt");
+	ofstream writefile(fileName);
 	for (size_t i = 0; i < name_vector.size(); i++) {
 		writefile << name_vector[i];
 		writefile << '\n';
@@ -49,28 +49,28 @@ void organize_list() {
 /* Function that deals with adding to the list.
 	 Also deals with ensuring that the list itself
 	 does not have a dupe. */
-void add_list() {
-	ifstream fileCheck("anime_index.txt");
+void add_list(string fileName) {
+	ifstream fileCheck(fileName);
 	fstream myfile;
 	ifstream checkfile;
 
 	// Checking to make sure file exists.
 	if (!fileCheck) {
-		ofstream fileOpen("anime_index.txt");
+		ofstream fileOpen(fileName);
 		cout << "No index detected. Currently making index.\n";
 		fileCheck.close();
 		fileOpen.close();
 	}
 
 	// Asking for input
-	myfile.open("anime_index.txt", std::fstream::app);
+	myfile.open(fileName, std::fstream::app);
 	string entry;
 	string textHolder;
 	cout << "Please put in an anime entry into the list:\n";
 	getline(cin, entry);
 
 	// Check for existing line or not.
-	checkfile.open("anime_index.txt", std::fstream::app);
+	checkfile.open(fileName, std::fstream::app);
 	while (!checkfile.eof()) {
 		getline(checkfile,textHolder);
 		if((textHolder == entry)) {
@@ -88,19 +88,19 @@ void add_list() {
 	// Closes Files and then organizes.
 	myfile.close();
 	checkfile.close();
-	organize_list();
+	organize_list(fileName);
 }
 
 /* Function that displays the list of anime names
 	 added to the current created list. */
-void display_list() {
-	ifstream fileCheck("anime_index.txt");
+void display_list(string fileName) {
+	ifstream fileCheck(fileName);
 	fstream myfile;
 	string name;
 
 	// File checking here.
 	if (!fileCheck) {
-		ofstream fileOpen("anime_index.txt");
+		ofstream fileOpen(fileName);
 		cout << "No index detected. A file will be made for you.\n";
 		cout << "Because this is a blank file, I will be returning you to the menu.\n";
 		fileCheck.close();
@@ -110,7 +110,7 @@ void display_list() {
 
 	// This prints the list out. File closes after.
 	else {
-		myfile.open("anime_index.txt");
+		myfile.open(fileName);
 		cout << "\nThis is your list of anime as of the moment.\n";
 		while (getline(myfile, name)) {
 			cout << name << endl;
@@ -122,14 +122,14 @@ void display_list() {
 /* Delete function that mainly uses a vector to hold data.
  	 Takes in user input + deletes says input from vector.
 	 File is overwritten with new vector. */
-void delete_entry() {
+void delete_entry(string fileName) {
 	string name;
 	string userChoice;
 	vector<string> name_vector;
 	bool token = false;
 
 	// Reading first and then making vector: prep for deletion
-	ifstream myfile("anime_index.txt");
+	ifstream myfile(fileName);
 	while (getline(myfile, name)) {
 		name_vector.push_back(name);
 	}
@@ -139,7 +139,7 @@ void delete_entry() {
 	// While loops that handles inputs + vector + writing of file. Uses Bool to hold
 	while (!token) {
 		// Input
-		display_list();
+		display_list(fileName);
 		cout << "\nWhat entry do you want to delete from your list?\n";
 		cin >> userChoice;
 		cin.ignore();
@@ -152,7 +152,7 @@ void delete_entry() {
 			cout << "Deleting Entry.\n";
 			token = true;
 			name_vector.erase(position);
-			ofstream writefile("anime_index.txt");
+			ofstream writefile(fileName);
 			for (int i = 0; i < name_vector.size(); i++) {
 				writefile << name_vector[i];
 				writefile << "\n";
@@ -169,6 +169,7 @@ void delete_entry() {
 int main() {
 	// Switch statements for inputs.
 	bool token = true;
+	string fileName = "anime_index.txt"
 	while (token) {
 		intro();
 		string choice_num;
@@ -191,17 +192,17 @@ int main() {
 		}
 
 		// Switch statement that defines our integer inputs.
-		// NEXT TO DO. Add a actual index system w/ user input name as well. 
+		// NEXT TO DO. Add a actual index system w/ user input name as well.
 		switch(actual_num) {
 			case 1:
 				// Maybe a more flexible way to add and organize at the same time?
-				add_list();
+				add_list(fileName);
 				break;
 			case 2:
-				display_list();
+				display_list(fileName);
 				break;
 			case 3:
-				delete_entry();
+				delete_entry(fileName);
 				break;
 			case 4:
 				token = false;
